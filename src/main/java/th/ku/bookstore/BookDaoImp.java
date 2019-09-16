24ff0b69bbf941d44fefa855eb903eb731c596e3
Methawee Punkaew <methawee.pu@ku.th>
2019-09-16 12:48:20 +0700

    first Commit

---
+++ src/main/java/th/ku/bookstore/BookDaoImp.java
@@ -0,0 +1,49 @@
+package th.ku.bookstore;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class BookDaoImp implements BookDao {
+    private JdbcTemplate jdbcTemplate;
+
+    public BookDaoImp(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(Book book) {
+        String query = "INSERT INTO book (id, name, price) VALUES (?, ?, ?);";
+        Object[] data = new Object[]
+                { book.getId(), book.getName(), book.getPrice() };
+        jdbcTemplate.update(query, data);
+
+
+    }
+
+    public void update(int id, Book book) {
+        String query = "UPDATE book SET name = ?, price=? WHERE id = "+id;
+        Object[] data = new Object[]
+                { book.getName(), book.getPrice() };
+        jdbcTemplate.update(query, data);
+    }
+
+    public void deleteById(int id) {
+        String query = "DELETE FROM book WHERE id = "+id;
+//        Object[] data = new Object[]
+//                {  };
+        jdbcTemplate.update(query);
+
+    }
+
+    public Book findById(int id) {
+        String query = "SELECT * FROM book WHERE id = " + id;
+        Book book = jdbcTemplate.queryForObject(query, new BookRowMapper());
+        return book;
+    }
+
+    public List<Book> findAll() {
+        String query = "SELECT * FROM book";
+        List<Book> books = jdbcTemplate.query(query, new BookRowMapper());
+        return books;
+    }
+}
